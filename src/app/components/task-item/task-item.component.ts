714261b88369c3e7400a8e2825a1e6d7439dd832
Amal122200 <amalmurikkoli2000@gmail.com>
2022-07-16 07:36:12 +0530

    task service get and delete and json-server

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = { text: "", day: "", reminder: false };
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  faTimes = faTimes
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+}
